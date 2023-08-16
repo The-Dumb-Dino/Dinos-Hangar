d81ac1696cafeb7df0aff0d26caddc4ed5aa8c7a
ChronoVortex <nick@hudsucker.com>
2023-08-15 23:45:48 -0700

    completed mantis warship achievements

--- data/dino_scripts/ships-ach.lua
+++ data/dino_scripts/ships-ach.lua
@@ -12,15 +12,6 @@ local function vter(cvec)
     end
 end
 
--- Check if a weapon's current shot is its first
-local function is_first_shot(weapon, afterFirstShot)
-    local shots = weapon.numShots
-    if weapon.weaponVisual.iChargeLevels > 0 then shots = shots*(weapon.weaponVisual.boostLevel + 1) end
-    if weapon.blueprint.miniProjectiles:size() > 0 then shots = shots*weapon.blueprint.miniProjectiles:size() end
-    if afterFirstShot then shots = shots - 1 end
-    return shots == weapon.queuedProjectiles:size()
-end
-
 -- Get a table for a userdata value by name
 local function userdata_table(userdata, tableName)
     if not userdata.table[tableName] then userdata.table[tableName] = {} end
@@ -110,6 +101,11 @@ end)
 -- MANTIS WARSHIP --
 --------------------
 
+local function crew_is_mantis(crew)
+    local crewSpecies = crew:GetSpecies()
+    return string_starts(crewSpecies, "mantis") or crewSpecies == "unique_kaz" or crewSpecies == "unique_freddy"
+end
+
 -- Easy
 script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(ship)
     if ship.iShipId == 0 and in_ship_combat(ship, Hyperspace.ships.enemy) and should_track_achievement("ACH_SHIP_MANTIS_WARSHIP_1", ship, "PLAYER_SHIP_MANTIS_WARSHIP") then
@@ -127,7 +123,66 @@ script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(ship)
 end)
 
 -- Normal
-
+script.on_internal_event(Defines.InternalEvents.DAMAGE_SYSTEM, function(ship, projectile, roomId, damage)
+    if ship.iShipId == 0 and damage.iSystemDamage + damage.iDamage < 0 and should_track_achievement("ACH_SHIP_MANTIS_WARSHIP_2", ship, "PLAYER_SHIP_MANTIS_WARSHIP") then -- Trash all counters if system repaired by projectile
+        local sysData = userdata_table(ship:GetSystemInRoom(roomId), "mods.dino.achTrackSys")
+        if sysData.repairTrackers then
+            for i, _ in pairs(sysData.repairTrackers) do
+                sysData.repairTrackers[i] = nil
+            end
+        end
+    end
+end)
+script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(ship)
+    if ship.iShipId == 0 and should_track_achievement("ACH_SHIP_MANTIS_WARSHIP_2", ship, "PLAYER_SHIP_MANTIS_WARSHIP") then
+        local nonMantisSystems = {} -- Find all systems that have non-mantis crew
+        for crew in vter(ship.vCrewList) do
+            local crewSystem = ship:GetSystemInRoom(crew.iRoomId)
+            if crewSystem and crew.iShipId == 0 and not crew_is_mantis(crew) then
+                nonMantisSystems[crewSystem:GetId()] = true
+            end
+        end
+        for system in vter(ship.vSystemList) do
+            if ship:HasSystem(system:GetId()) then
+                local sysData = userdata_table(system, "mods.dino.achTrackSys")
+                if nonMantisSystems[system:GetId()] then -- Trash all counters if there are non-mantis in this system
+                    if sysData.repairTrackers then
+                        for i, _ in pairs(sysData.repairTrackers) do
+                            sysData.repairTrackers[i] = nil
+                        end
+                    end
+                else
+                    if sysData.repairTrackers then -- Time repair counters
+                        for i, repairTracker in pairs(sysData.repairTrackers) do
+                            repairTracker.timer = repairTracker.timer + Hyperspace.FPS.SpeedFactor/16
+                            if repairTracker.timer >= 10 then
+                                sysData.repairTrackers[i] = nil
+                            end
+                        end
+                    end
+                    if sysData.damageChange and sysData.damageChange < 0 then -- Track repairs with counters
+                        if not sysData.repairTrackers then
+                            sysData.repairTrackers = {}
+                        else
+                            for _, repairTracker in pairs(sysData.repairTrackers) do
+                                repairTracker.count = repairTracker.count - sysData.damageChange
+                                if repairTracker.count >= 6 then
+                                    Hyperspace.CustomAchievementTracker.instance:SetAchievement("ACH_SHIP_MANTIS_WARSHIP_2", false)
+                                    sysData.repairTrackers = nil
+                                    return
+                                end
+                            end
+                        end
+                        table.insert(sysData.repairTrackers, {
+                            timer = 0,
+                            count = -sysData.damageChange
+                        })
+                    end
+                end
+            end
+        end
+    end
+end)
 
 -- Hard
 local combatTimer = 0
@@ -158,8 +213,7 @@ script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(ship)
                 local boarders = false
                 for crew in vter(enemyShip.vCrewList) do
                     if crew.iShipId == 0 then
-                        local crewSpecies = crew:GetSpecies()
-                        if not (string_starts(crewSpecies, "mantis") or crewSpecies == "unique_kaz" or crewSpecies == "unique_freddy") then
+                        if not crew_is_mantis(crew) then
                             vars.loc_ach_mantis_only_boarders = 0 -- Invalidate if any boarders aren't mantis
                             return
                         end
